0710b0fbecc478e16c79e9335080d9759fc5be31
phillip <nzauonline@gmail.com>
2023-02-20 21:57:39 +0300

    home page setup

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard.component';
 import {HomeComponent} from "./components/home/home.component";
 
 const routes: Routes = [

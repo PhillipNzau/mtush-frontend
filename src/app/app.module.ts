207eb3fff4aead2a259e3c5d38d7c68aa30d7258
phillip <nzauonline@gmail.com>
2023-02-20 20:09:12 +0300

    page not found and coming soon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComingSoonComponent } from './shared/components/coming-soon/coming-soon.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComingSoonComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,

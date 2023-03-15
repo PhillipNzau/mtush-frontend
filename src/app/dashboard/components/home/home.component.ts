33de1969ab0d24ae820d2e633c10156cbfd9537a
phillipnzau <nzauonline78@gmail.com>
2023-03-15 12:45:38 +0300

    Menu add the icons and close func

--- src/app/dashboard/components/home/home.component.ts
+++ src/app/dashboard/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ItemCard } from 'src/app/shared/models/item-card';
 
 
@@ -7,27 +7,36 @@ import { ItemCard } from 'src/app/shared/models/item-card';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-items:ItemCard[] = [
-  {
-    id:1,
-    price:240,
-    brand: 'Nike',
-    size: 'XL',
-    location: 'Kenya, Nairobi',
-    liked: true,
-    numberOfLikes: 2,
-  },
-  {
-    id:2,
-    price:300,
-    brand: 'Adidas',
-    size: 'L',
-    location: 'Kenya, Nairobi',
-    liked: false,
-    numberOfLikes: 7,
-  }
-];
+export class HomeComponent implements OnInit {
+  items:ItemCard[] = [
+    {
+      id:1,
+      price:240,
+      brand: 'Nike',
+      size: 'XL',
+      location: 'Kenya, Nairobi',
+      liked: true,
+      numberOfLikes: 2,
+    },
+    {
+      id:2,
+      price:300,
+      brand: 'Adidas',
+      size: 'L',
+      location: 'Kenya, Nairobi',
+      liked: false,
+      numberOfLikes: 7,
+    }
+  ];  
+  brands: string[] = ['Nike', 'Adidas', 'Ralph Lauren', 'New Balance', 'Puma', 'Vans', 'Burberry', 'Tommy Hilfiger', 'Reebok'];
+  isMenuOpen:boolean=false;
+  
 
-brands: string[] = ['Nike', 'Adidas', 'Ralph Lauren', 'New Balance', 'Puma', 'Vans', 'Burberry', 'Tommy Hilfiger', 'Reebok']
+  constructor() { }
+  ngOnInit(): void {
+    
+  }
+  menuOpen() {
+    this.isMenuOpen = !this.isMenuOpen
+  }
 }

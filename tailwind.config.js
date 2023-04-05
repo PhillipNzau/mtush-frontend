dc4062a3dd8a9a8dc2854d97dd67424c1e33e01c
phillipnzau <nzauonline78@gmail.com>
2023-04-05 11:56:00 +0300

    add item page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primaryYellow: '#F7D861',
-        primaryGreen: '#2B5B5D',
-        secondaryGreen: '#154726',
-        orange: '#F1753F',
-        textYellow: '#E8AF02'
+        primaryYellow: 'hsl(48, 90%, 67%)',
+        primaryGreen: 'hsl(171, 58%, 22%)',
+        secondaryGreen: 'hsl(140, 54%, 18% )',
+        lightGreen: 'hsla(152, 13%, 64%, 1)',
+        orange: 'hsl(18, 86%, 60%)',
+        textYellow: 'hsl(45, 98%, 46%)',
+        primaryWhite: 'hsl(0, 0%, 100%)',
+        primaryBg: 'hsl(39, 67%, 94%)',
       },
       fontFamily: {
         'textFont': ['FoundersRegular', 'sans-serif'],
